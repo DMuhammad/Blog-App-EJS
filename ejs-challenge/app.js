b1e47c3c99d345c5ca82bc22d249f3ee92dfe8ba
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-24 11:09:34 +0700

    complete step 2

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -14,7 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    content: homeStartingContent
+  });
 })
 
 
