94968c7f8666b7415cc6616ccd2604e043c826ae
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-29 09:04:24 +0700

    save composed posts with mongoDB

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,6 +11,14 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const posts = [];
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/blogAppDB", {useNewUrlParser: true});
+const contentSchema = {
+  title: String,
+  content: String
+};
+
+const Content = mongoose.model("content", contentSchema);
 
 app.set('view engine', 'ejs');
 
@@ -51,12 +60,12 @@ app.get("/posts/:post", (req, res) => {
 })
 
 app.post("/compose", (req, res) => {
-  const post = {
-    "title": req.body.title,
-    "content": req.body.content
-  }
-  posts.push(post);
-  res.redirect("/");
+  const post = new Content ({
+    title: req.body.title,
+    content: req.body.content
+  })
+  post.save();
+  // res.redirect("/");
 })
 
 
